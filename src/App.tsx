c106b13b9dd16b8c9bd90377e806343f92f91ebc
David Van Osdol <davidvanosdol88@gmail.com>
2025-12-29 09:52:00 -0500

    feat: enable routing and update dvo88 landing page link to Capacity Calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ConstructionZone from './ConstructionZone';
+import CapacityCalculator from './components/CapacityCalculator';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
-      <ConstructionZone />
-    </div>
+    <HashRouter>
+      <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
+        <Routes>
+          <Route path="/" element={<ConstructionZone />} />
+          <Route path="/calculator" element={<CapacityCalculator />} />
+          <Route path="*" element={<ConstructionZone />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
